c72e1b1ec39744bcd687021d20eb2f234a187646
Robson Oliveira da Costa <robsondeveloper@gmail.com>
2021-02-04 10:26:06 -0400

    exibindo capas do album na pagina de detalhe

--- src/views/AlbumDetail.vue
+++ src/views/AlbumDetail.vue
@@ -2,7 +2,20 @@
   <div>
     <b-card header="Álbum" header-tag="header" :title="album.name">
       <b-card-text>{{ album.artist.name }}</b-card-text>
+      <b-container fluid class="p-2 bg-dark" v-if="album.covers.length">
+        <b-row>
+          <b-col
+            v-for="item in album.covers"
+            :img-src="item.url"
+            :key="item.id"
+          >
+            <b-img thumbnail fluid :src="item.url" width="256" height="256">
+            </b-img>
+          </b-col>
+        </b-row>
+      </b-container>
     </b-card>
+
     <hr class="my-4" />
     <router-link :to="{ path: 'album-search', query: { search: search } }">
       Voltar para Pesquisa
